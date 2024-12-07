7da62bf4ee75502156ce7d522f2d896f3ee2fb74
jamal-o <jamaldeenolasupo@gmail.com>
2024-12-07 22:14:00 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(color: Colors.green),
         ),
       ),
     );
